69946ad111df9f2abffcaead2160c57015bb0ae3
EliCove <marioncovington@hotmail.com>
2024-04-27 22:28:23 -0700

    completed credit card checker and bonus challenges

--- credit-card-checker/main.js
+++ credit-card-checker/main.js
@@ -20,11 +20,25 @@ const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
 const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
 
 // An array of all the arrays above
-const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
-
+let batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
+let testNumbers = ['4716723033016654', '4539364833164660', '4539182292700044031', '5194727635742391', '2720996056716388', '5401082474200534', '372945456263025', '343202122558735', '370078786115277', '6011628311453981', '6011447918286763', '6011266562786753664'];
+let testNumbersTwoFalse = ['3544171939504525', '344801968305414'];
 
 // Add your functions below:
-const validateCred = numArr => {
+
+//Converts a string array to a number array:
+const strToNumArray = str => {
+  const newArray = str.split('');
+  let numArray = [];
+  for (const character of newArray){
+    numArray.push(+character);
+  }
+  return numArray;
+};
+
+
+//gets the sum of the cards numbers ready to check if valid or not:
+const validCheckerSum = numArr => {
   let even = 0;
   let sumArr = numArr.toReversed();
 
@@ -42,19 +56,145 @@ const validateCred = numArr => {
     }
   });
 
-  const sum = sumArr.reduce((accum, currentVal) => accum + currentVal);
+  return sumArr.reduce((accum, currentVal) => accum + currentVal);
+}
+//console.log(strToNumArray('4716723033016654'));
+
+
+//checks if a card is valid and returns true or false:
+const validateCred = numArr => {
+  const sum = validCheckerSum(numArr);
 
   if (sum % 10 === 0) {
     return true;
   }
   return false;
 };
-
 //console.log(validateCred(mystery2));
 
+
+//checks nested arrays for invalid cards and returns a nested array of invalid cards:
 const findInvalidCards = numNestArr => {
-  console.log(numNestArr);
+  let invalidCards = [];
+  for (let card of numNestArr) {
+    if (typeof card === 'string'){
+      card = strToNumArray(card);
+    }
+    if (!validateCred(card)) {
+      invalidCards.push(card);
+    }
+  }
+  return invalidCards;
 };
+//console.log(findInvalidCards(batch));
+//console.log(findInvalidCards(testNumbers));
+//console.log(findInvalidCards(testNumbersTwo));
+
+
+
+//takes a nested array of invalid cards and returns an array of known companies have issued those cards out
 const idInvalidCardCompanies = numNestArr => {
-  console.log(numNestArr);
+  let invalidCardCompanies = [];
+  for (const card of numNestArr) {
+    if (card[0] === 3  && card[1] === 7 && !invalidCardCompanies.includes('Amek (American Express)')){
+      invalidCardCompanies.push('Amek (American Express)');
+    } else if (card[0] === 4 && !invalidCardCompanies.includes('Visa')){
+      invalidCardCompanies.push('Visa');
+    } else if (card[0] === 5 && !invalidCardCompanies.includes('MasterCard')){
+      invalidCardCompanies.push('MasterCard');
+    } else if (card[0] === 6 && !invalidCardCompanies.includes('Discover')){
+      invalidCardCompanies.push('Discover');
+    } else if (!(card[0] === 3 && card[1] == 7 || card[0] === 4 || card[0] === 5 || card[0] === 6)) {
+      //invalidCardCompanies.push('Company not found');
+      console.log('Company not found');
+    }
+  }
+  return invalidCardCompanies;
 };
+//console.log(idInvalidCardCompanies(findInvalidCards(batch)));
+//console.log(idInvalidCardCompanies(findInvalidCards(testNumbers)));
+//console.log(idInvalidCardCompanies(findInvalidCards(testNumbersTwo)));
+
+
+
+//takes in an invalid card array and converts it into new valid card, which is returned:
+const convertInvalid = invalidCardArray => {
+  let card = invalidCardArray.slice();
+  const sum = validCheckerSum(invalidCardArray);
+  const thirdLast = card.length - 3;
+  const fifthLast = card.length - 5;
+  const seventhLast = card.length - 7;
+  const ninthLast = card.length - 9;
+  const eleventhLast = card.length - 11;
+
+  const repeatCheckSub = caseNum => {
+    if (card[thirdLast] > (caseNum - 1)) {
+      card[thirdLast] -= caseNum;
+    } else if (card[fifthLast] > (caseNum - 1)) {
+      card[fifthLast] -= caseNum;
+    } else if (card[seventhLast] > (caseNum - 1)) {
+      card[seventhLast] -= caseNum;
+    } else if (card[ninthLast] > (caseNum - 1)) {
+      card[ninthLast] -= caseNum;
+    } else {
+      card[eleventhLast] -= caseNum;
+    }
+  };
+  const repeatCheckAdd = caseNum => {
+    if (card[thirdLast] < caseNum) {
+        card[thirdLast] += (10 - caseNum);
+      } else if (card[fifthLast] < caseNum) {
+        card[fifthLast] += (10 - caseNum);
+      } else if (card[seventhLast] < caseNum) {
+        card[seventhLast] += (10 - caseNum);
+      } else if (card[ninthLast] < caseNum) {
+        card[ninthLast] += (10 - caseNum);
+      } else {
+        card[eleventhLast] += (10 - caseNum);
+      }
+  };
+
+  switch (sum % 10) {
+    case 1:
+      repeatCheckSub(1);
+      break;
+    case 2:
+      repeatCheckSub(2);
+      break;
+    case 3:
+      repeatCheckSub(3);
+      break;
+    case 4:
+      repeatCheckSub(4);
+      break;
+    case 5:
+      repeatCheckSub(5);
+      break;
+    case 6:
+      repeatCheckAdd(6);
+      break;
+    case 7:
+      repeatCheckAdd(7);
+      break;
+    case 8: 
+      repeatCheckAdd(8);
+      break;
+    case 9:
+      repeatCheckAdd(9);
+      break;
+    default:
+      return 'This is not an invalid card';
+  }
+    return card;
+};
+
+
+
+const newValidCard1 = convertInvalid(invalid1);
+const newValidCard2 = convertInvalid(invalid2);
+
+console.log(newValidCard1);
+console.log(validateCred(newValidCard1));
+
+console.log(newValidCard2);
+console.log(validateCred(newValidCard2));
